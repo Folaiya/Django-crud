ba6fcdd22e9eda6f3389415529d9317574a4358a
Folaiya <folatech21@gmail.com>
2022-06-24 21:24:38 +0100

    Made some changes to the project git commit -m 'Made some changes to the project

---
+++ folashadeolaiya/blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView ,CreateView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView 
+from .models import Post
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields =  ['all']
+
+    success_url  = reverse_lazy('blog:all')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['all']
+
+    success_url  = reverse_lazy('blog:all')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = ['all']
+
+    success_url  = reverse_lazy('blog:all')
